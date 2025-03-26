a7a203e2b7fc0e2ff1926893aad170d61416f008
FeynmanFan <cbehrens@gmail.com>
2025-03-26 13:56:16 -0500

    Added new tests for thresholds, and adjusted tests to use the constants

--- c-sharp/ThermalResponse.Tests/SystemStatusTests.cs
+++ c-sharp/ThermalResponse.Tests/SystemStatusTests.cs
@@ -6,7 +6,7 @@
         public void BelowMin_NeedsHeater()
         {
             var system = new SystemStatus();
-            var temp = 150m; // ice, ice baby
+            var temp = SystemStatus.MINIMUM_TEMPERATURE - 1; // ice, ice baby
 
             var result = system.CheckTemp(temp);
 
@@ -17,7 +17,7 @@
         public void AboveMin_Nominal()
         {
             var system = new SystemStatus();
-            var temp = 400m; // 126C
+            var temp = SystemStatus.MINIMUM_TEMPERATURE + 1; // 126C
 
             var result = system.CheckTemp(temp);
 
@@ -28,11 +28,40 @@
         public void AboveMax_NeedsCooling()
         {
             var system = new SystemStatus();
-            var temp = 730m; // super-hot
+            var temp = SystemStatus.MAXIMUM_TEMPERATURE + 1; // super-hot
 
             var result = system.CheckTemp(temp);
 
             Assert.Equal(TemperatureStatus.CoolingNeeded, result);
+        }
+
+        [Fact]
+        public void AtMax_Nominal()
+        {
+            // arrange
+            var system = new SystemStatus();
+            var temp = SystemStatus.MAXIMUM_TEMPERATURE;
+
+            // act
+            var result = system.CheckTemp(temp);
+
+            // assert
+            Assert.Equal(TemperatureStatus.Nominal, result);
+        }
+
+
+        [Fact]
+        public void AtMin_Nominal()
+        {
+            // arrange
+            var system = new SystemStatus();
+            var temp = SystemStatus.MINIMUM_TEMPERATURE;
+
+            // act
+            var result = system.CheckTemp(temp);
+
+            // assert
+            Assert.Equal(TemperatureStatus.Nominal, result);
         }
     }
 }
